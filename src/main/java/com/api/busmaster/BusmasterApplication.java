37b3d4c12e5c3ebcedf5ae6a1672678874d63ecf
SimpleGeek <SimpleGeek@github.com>
2020-06-14 19:11:16 -0400

    (nitial commit

---
+++ src/main/java/com/api/busmaster/BusmasterApplication.java
@@ -0,0 +1,13 @@
+package com.api.busmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusmasterApplication.class, args);
+	}
+
+}
