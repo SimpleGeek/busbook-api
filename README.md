78c7f3fb32b550d3e45166265e21cb3e62ef5f1f
SimpleGeek <SimpleGeek@github.com>
2020-07-11 11:23:12 -0400

    Added db creds, updated readme, fixed controller issue

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ This is the api for the busmaster project.
 ### Running the Postgresql image for this app
 The schema and default test data are stored in schema.sql and data.sql respectively under the src/main/resources dir of this program.
 To start postgres using podman, use this command: 
-podman run --name busmaster-postgres -e POSTGRES_PASSWORD=buspw -d postgres
+podman run --name some-postgres -p 5432:5432 -e POSTGRES_PASSWORD=buspw -d postgres
 
 Note that the default user is 'postgres'.  The command above starts the postgres server with the default schema of 'postgres' and a password of 'buspw'.
--- src/main/java/com/api/busmaster/controllers/RiderController.java
+++ src/main/java/com/api/busmaster/controllers/RiderController.java
@@ -1,13 +1,13 @@
 package com.api.busmaster.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.api.busmaster.models.Rider;
 import com.api.busmaster.services.RiderService;
 
-@Controller
+@RestController
 public class RiderController {
 	@Autowired
 	private RiderService riderService;
@@ -15,5 +15,10 @@ public class RiderController {
 	@GetMapping("/api/getrider")
 	public Rider getRider() {
 		return riderService.getRider();
+	}
+	
+	@GetMapping("/hello")
+	public String getHello() {
+		return "Hello!";
 	}
 }
