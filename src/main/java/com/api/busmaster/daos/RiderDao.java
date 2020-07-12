ae5e80bec1ee87da2bb5e6c564618b1d430d3ca2
SimpleGeek <SimpleGeek@github.com>
2020-07-12 13:19:16 -0400

    Updated rider and stop daos

--- src/main/java/com/api/busmaster/daos/RiderDao.java
+++ src/main/java/com/api/busmaster/daos/RiderDao.java
@@ -27,18 +27,45 @@ public class RiderDao {
 				"from riders";
 		SqlRowSet rs = jdbcTemplate.queryForRowSet(getRiderSql);
 		while(rs.next()) {
-			// The postgres AGE function returns the age in a string formatted like so:
-			// XX years XX mons XX days
-			// In this case, we only care about the years, so we're simply pulling out
-			// the first two characters of that string.
-			int age = Integer.parseInt(rs.getString("age_str").substring(0, 2).trim());
-			riders.add(new Rider(rs.getInt("rider_id"), rs.getString("fname"),
-								 rs.getString("lname"), age, rs.getString("birthday"), rs.getInt("stop_id")));
+			riders.add(new Rider(rs.getInt("rider_id"), rs.getString("fname"), rs.getString("lname"), 
+								 getYearsFromAgeStr(rs.getString("age_str")), rs.getString("birthday"), rs.getInt("stop_id")));
 		}
 		return riders;
 	}
 	
 	public List<Rider> getRidersForStop(int stopId) {
-		return new ArrayList<Rider>();
+		String getRidersForStopSql =
+				"select rider_id,\n" +
+				"       fname,\n" +
+				"       lname,\n" +
+				"       age(birthday) as age_str,\n" +
+				"		to_char(birthday, 'Mon dd, yyyy') as birthday,\n" +
+				"       stop_id\n" +
+				"  from riders\n" +
+				" where stop_id = ?";
+		
+		List<Rider> stopRiders = new ArrayList<Rider>();
+		SqlRowSet rs = jdbcTemplate.queryForRowSet(getRidersForStopSql,stopId);
+		while (rs.next()) {
+			stopRiders.add(new Rider(rs.getInt("rider_id"), rs.getString("fname"), rs.getString("lname"), 
+									 getYearsFromAgeStr(rs.getString("age_str")), rs.getString("birthday"), rs.getInt("stop_id")));
+		}
+		return stopRiders;
+	}
+	
+	// Private utility methods
+	/**
+	 * This method accepts the output of a postgres AGE function call as a string,
+	 * and returns an integer for the age in years.
+	 * 
+	 * @param ageStr
+	 * @return
+	 */
+	private int getYearsFromAgeStr(String ageStr) {
+		// The postgres AGE function returns the age in a string formatted like so:
+		// XX years XX mons XX days
+		// In this case, we only care about the years, so we're simply pulling out
+		// the first two characters of that string.
+		return Integer.parseInt(ageStr.substring(0, 2).trim());
 	}
 }
--- src/main/java/com/api/busmaster/daos/StopDao.java
+++ src/main/java/com/api/busmaster/daos/StopDao.java
@@ -2,6 +2,7 @@ package com.api.busmaster.daos;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 
 import com.api.busmaster.models.Stop;
@@ -11,9 +12,44 @@ public class StopDao {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
-	public Stop getNextStop(int prevStopSeqNum) {
+	/**
+	 * This method returns the next stop in a route, given the sequence
+	 * number of the previous stop, and the id of the route.
+	 * 
+	 * @param prevStopSeqNum
+	 * @param routeId
+	 * @return
+	 */
+	public Stop getNextStop(int prevStopSeqNum, int routeId) {
 		String getNextStopSql =
-				"select";
-		return new Stop();
+				"select stop_id,\n" +
+				"       seq_num,\n" +
+				"       street_addr,\n" +
+				"       city,\n" +
+				"       state_abbr,\n" +
+				"       zip,\n" +
+				"       apartment,\n" +
+				"       building,\n" +
+				"       door,\n" +
+				"       route_id\n" +
+				"  from stops\n" +
+				" where seq_num = ?\n" +
+				"   and route_id = ?";
+		
+		SqlRowSet rs = jdbcTemplate.queryForRowSet(getNextStopSql, (prevStopSeqNum + 1), routeId);
+		Stop nextStop = new Stop();
+		while (rs.next()) {
+			nextStop.setStopid(rs.getInt("stop_id"));
+			nextStop.setSeqNum(rs.getInt("seq_num"));
+			nextStop.setStreetAddr(rs.getString("street_addr"));
+			nextStop.setCity(rs.getString("city"));
+			nextStop.setStateAbbr(rs.getString("state_abbr"));
+			nextStop.setZip(rs.getString("zip"));
+			nextStop.setApartment(rs.getString("apartment"));
+			nextStop.setBuilding(rs.getString("building"));
+			nextStop.setDoor(rs.getString("door"));
+			nextStop.setRouteId(rs.getInt("route_id"));
+		}
+		return nextStop;
 	}
 }
